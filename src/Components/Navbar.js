8a10638ad3467155bff3d54a91f1edd0d58b2df2
omborate100 <omcode100@gmail.com>
2023-10-01 20:07:14 +0530

    first commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,7 +14,7 @@ export class Navbar extends Component {
   render() {
     return (
        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-  <a className="navbar-brand" to="#">Navbar</a>
+  <a className="navbar-brand" to="#">Instant News</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon" />
   </button>
